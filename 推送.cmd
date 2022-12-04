391db7b0e620fe57d9e13b7fcb01f69697811dad
xX7912Xx <wzm0529@163.com>
2022-12-04 14:23:17 +0800

    初始化

--- 推送.cmd
+++ 推送.cmd
@@ -1,16 +1,17 @@
+chcp 65001
 @echo off
-echo DotCS - Uploading files to Github.
+echo DotCS - 上传文件至 Github.
 git config --global core.autocrlf false
-set /p commit=Please enter your commit info: 
-echo Loading files.
+set /p commit=请输入提交说明: 
+echo 加载文件.
 git update-index --add --chmod=+x .\build\upx\Linux_amd64\upx
 git update-index --add --chmod=+x .\build\upx\Linux_aarch64\upx
 git add .
-echo Setting commit.
+echo 设置提交说明.
 git commit -m "%commit%"
-echo Setting branch.
+echo 设置分支.
 git branch -M "v1.0.0"
-echo Pushing.
+echo 推送.
 git push -u DotCS "v1.0.0"
-echo Finished.
+echo 已完成.
 pause
